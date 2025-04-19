685f56cd6644db0625be6c6ff49a16e850653db6
bautistaflores <bautistaflores22@gmail.com>
2025-04-19 20:14:46 -0300

    Responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 import bgMainDesktop from './assets/images/bg-main-desktop.png';
 import bgMainMobile from './assets/images/bg-main-mobile.png';
 import cardFront from './assets/images/bg-card-front.png';
 import cardBack from './assets/images/bg-card-back.png';
 import cardLogo from './assets/images/card-logo.svg';
-import favicon from './assets/images/favicon-32x32.png';
 import iconComplete from './assets/images/icon-complete.svg';
 
 
 function App() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const { 
     register, 
@@ -21,6 +22,7 @@ function App() {
 
   const onSubmit = (data) => {
     console.log(data);
+    setIsSubmitted(true);
   }
 
   const formatCardNumber = (value) => {
@@ -32,54 +34,57 @@ function App() {
   };
 
   return (
-
-    <div className='flex h-screen'>
+    <div className='sm:flex min-h-screen'>
       
       {/* Imagenes */}
-      <div className='relative w-1/2 overflow-hidden'>
-        <img src={bgMainDesktop} className='absolute inset-0 h-full object-cover'/>
+      <div className='relative sm:w-1/2 overflow-hidden'>
+        <img src={bgMainMobile} className='block absolute sm:hidden w-full h-[300px] object-cover'/>
+        <img src={bgMainDesktop} className='absolute inset-0 h-full object-cover hidden sm:block'/>
 
         {/* Tarjetas */}
-        <div className='relative h-full flex flex-col justify-center items-start px-16 gap-6'>
+        {/* <div className='relative py-20 sm:h-full flex flex-col justify-center items-start px-16 gap-6'> */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 relative">
 
           {/* Imagen trasera */}
-          <div className='relative top-25 left-50 z-0'>
-            <img src={cardBack} className='w-[450px]'/>
+          <div className="relative top-[15%] left-[26%] sm:top-[170%] sm:left-[55%]">
+            <img src={cardBack} className="w-[65%] sm:w-full" />
 
-            <div className="absolute top-27 left-90 text-white text-lg">
+            <div className="absolute top-[44.5%] left-[52%] sm:top-[44%] sm:left-[79%] text-white sm:text-[0.95vw]">
               {watch("cvc") || "000"}
             </div>
           </div>
 
           {/* Imagen delantera */}
-          <div className='absolute top-37 left-40 z-10'>
-            <img src={cardFront} className='w-[450px]'/>
+          <div className="relative top-[-28.5%] right-[-10%] sm:top-[50%] sm:right-[65%]">
+            <img src={cardFront} className='w-[65%] sm:w-full'/>
 
-            <div className="absolute top-39 left-12 text-white text-3xl">
+            {/* Logo */}
+            <div className='absolute top-[12%] left-[4%] sm:left-[6%] z-20'>
+              <img src={cardLogo} className='w-full h-auto'/>
+            </div>
+
+            <div className="absolute top-[57%] sm:top-[60%] left-[6%] sm:left-[10%] text-white text-[4.6vw] sm:text-[1.75vw] whitespace-nowrap">
               {watch("cardNumber") || "0000 0000 0000 0000"}
             </div>
 
-            <div className="absolute top-51 left-12 text-white text-md">
+            <div className="absolute top-[79%] sm:top-[83%] left-[6%] sm:left-[10%] text-white text-[2.4vw] sm:text-[0.85vw]">
               {watch("cardholderName") || "CARDHOLDER NAME"}
             </div>
 
-            <div className="absolute top-51 left-90 text-white text-md">
+            <div className="absolute top-[79%] sm:top-[83%] left-[53%] sm:left-[80%] text-white text-[2.4vw] sm:text-[0.85vw]">
               {watch("expMonth") || "00"}/{watch("expYear") || "00"}
             </div>
 
-          </div>
-
-          {/* Logo */}
-          <div className='absolute top-45 left-49 z-20'>
-            <img src={cardLogo} className='w-[90px]'/>
           </div>
         </div>
       </div> 
 
       {/* Formulario */}
-      <div className='w-1/2 flex items-center'>
+      <div className='sm:w-1/2 flex items-center justify-center py-10'>
+
+        {!isSubmitted ? (
         <form 
-          className="w-full max-w-md" 
+          className="w-full max-w-md sm:scale-110 scale-[90%]" 
           onSubmit={handleSubmit(onSubmit)}
         >
 
@@ -89,7 +94,8 @@ function App() {
             <input 
               type="text"
               {...register("cardholderName", { required: "Name is required" })}
-              className={`text-lg; ${errors.cardholderName ? 'border-red-500' : 'border-gray-300'}`}
+              placeholder="e.g. Jane Doe"
+              className={`text-lg ${errors.cardholderName ? 'border-red-500' : 'border-gray-300'}`}
             />
             {errors.cardholderName && <span className="text-red-500 mt-2">{errors.cardholderName.message}</span>}
 
@@ -111,7 +117,7 @@ function App() {
                 const formatted = formatCardNumber(e.target.value);
                 setValue("cardNumber", formatted); // Actualiza el valor en el form
               }}
-              className={`text-lg tracking-{3} ${errors.cardNumber ? 'border-red-500' : 'border-gray-300'}`}
+              className={`text-lg ${errors.cardNumber ? 'border-red-500' : 'border-gray-300'}`}
             />
             {errors.cardNumber && <span className="text-red-500 mt-2">{errors.cardNumber.message}</span>}
 
@@ -186,11 +192,24 @@ function App() {
           </div>
 
         </form>
+        ) : (
+          <div className="flex flex-col items-center text-center p-10 scale-125">
+            <img src={iconComplete} className="w-20 mb-8" />
+            <h2 className="text-4xl tracking-widest mb-4">THANK YOU!</h2>
+            <p className="text-gray-500 text-lg mb-8">We’ve added your card details</p>
+            <button 
+              onClick={() => setIsSubmitted(false)} 
+              className='bg-[#21092f] text-white rounded-md p-3 w-2/3'
+            >
+              Continue
+            </button>
+          </div>
+        )}
       </div>
-
     </div>
     
   )
 }
+
 
 export default App

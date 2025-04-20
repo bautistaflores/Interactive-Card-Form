14f415bba5cf7021663427e11db3d912c5826abe
bautistaflores <bautistaflores22@gmail.com>
2025-04-19 21:32:08 -0300

    Responsive mejoras

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -41,25 +41,25 @@ function App() {
         <img src={bgMainMobile} className='block absolute sm:hidden w-full h-[300px] object-cover'/>
         <img src={bgMainDesktop} className='absolute inset-0 h-full object-cover hidden sm:block'/>
 
+
         {/* Tarjetas */}
-        {/* <div className='relative py-20 sm:h-full flex flex-col justify-center items-start px-16 gap-6'> */}
         <div className="grid grid-cols-1 sm:grid-cols-2 relative">
 
           {/* Imagen trasera */}
           <div className="relative top-[15%] left-[26%] sm:top-[170%] sm:left-[55%]">
             <img src={cardBack} className="w-[65%] sm:w-full" />
 
-            <div className="absolute top-[44.5%] left-[52%] sm:top-[44%] sm:left-[79%] text-white sm:text-[0.95vw]">
+            <div className="absolute top-[43%] left-[51%] sm:top-[44%] sm:left-[79%] text-white text-[3vw] sm:text-[0.95vw]">
               {watch("cvc") || "000"}
             </div>
           </div>
 
           {/* Imagen delantera */}
-          <div className="relative top-[-28.5%] right-[-10%] sm:top-[50%] sm:right-[65%]">
-            <img src={cardFront} className='w-[65%] sm:w-full'/>
+          <div className="relative top-[-28.5%] right-[-10%] sm:top-[50%] sm:right-[65%] w-[65%] sm:w-full">
+            <img src={cardFront} className='w-full h-auto'/>
 
             {/* Logo */}
-            <div className='absolute top-[12%] left-[4%] sm:left-[6%] z-20'>
+            <div className='absolute top-[12%] left-[4%] sm:left-[6%] w-[20%]'>
               <img src={cardLogo} className='w-full h-auto'/>
             </div>
 
@@ -80,131 +80,133 @@ function App() {
       </div> 
 
       {/* Formulario */}
-      <div className='sm:w-1/2 flex items-center justify-center py-10'>
+      <div className='sm:w-1/2 flex items-center justify-center sm:justify-start py-10 px-4'>
+        <div className="w-full max-w-lg">
 
-        {!isSubmitted ? (
-        <form 
-          className="w-full max-w-md sm:scale-110 scale-[90%]" 
-          onSubmit={handleSubmit(onSubmit)}
-        >
+          {!isSubmitted ? (
+          <form 
+            className="w-full space-y-6"
+            onSubmit={handleSubmit(onSubmit)}
+          >
 
-          <div className="flex flex-col">
-            {/* CARDHOLDER NAME */}
-            <label className='tracking-wider mb-2'>CARDHOLDER NAME</label>
-            <input 
-              type="text"
-              {...register("cardholderName", { required: "Name is required" })}
-              placeholder="e.g. Jane Doe"
-              className={`text-lg ${errors.cardholderName ? 'border-red-500' : 'border-gray-300'}`}
-            />
-            {errors.cardholderName && <span className="text-red-500 mt-2">{errors.cardholderName.message}</span>}
-
-
-            {/* CARD NUMBER */}
-            <label className='tracking-wider mt-6 mb-2'>CARD NUMBER</label>
-            <input 
-              type="text" 
-              placeholder="0000 0000 0000 0000"
-              maxLength={19}
-              {...register("cardNumber", { 
-                required: "Card number is required", 
-                pattern: {
-                  value: /^(\d{4} ){3}\d{4}$/,
-                  message: "Invalid format"
-                } 
-              })}
-              onChange={(e) => {
-                const formatted = formatCardNumber(e.target.value);
-                setValue("cardNumber", formatted); // Actualiza el valor en el form
-              }}
-              className={`text-lg ${errors.cardNumber ? 'border-red-500' : 'border-gray-300'}`}
-            />
-            {errors.cardNumber && <span className="text-red-500 mt-2">{errors.cardNumber.message}</span>}
+            <div className="flex flex-col">
+              {/* CARDHOLDER NAME */}
+              <label className='tracking-wider mb-2'>CARDHOLDER NAME</label>
+              <input 
+                type="text"
+                {...register("cardholderName", { required: "Name is required" })}
+                placeholder="e.g. Jane Doe"
+                className={`text-lg ${errors.cardholderName ? 'border-red-500' : 'border-gray-300'}`}
+              />
+              {errors.cardholderName && <span className="text-red-500 mt-2">{errors.cardholderName.message}</span>}
 
 
-            {/* MONTH AND CVC */}
-            <div className="flex gap-4 mt-6">
-              <div className="flex-1">
-                <label className="block mb-2 tracking-wider">EXP. DATE (MM/YY)</label>
-                <div className='flex gap-2'>
-                  <input 
-                    type="text" 
-                    {...register("expMonth", {
-                      required: "Month is required",
-                      pattern: {
-                        value: /^(0[1-9]|1[0-2])$/,
-                        message: "Invalid month"
-                      }
-                    })}
-                    onChange={(e) => {
-                      const formatted = e.target.value.replace(/\D/g, "").slice(0, 2);
-                      setValue("expMonth", formatted); // Actualiza el valor en el form
-                    }}
-                    className='w-full text-lg' 
-                    placeholder='MM'
-                  />
+              {/* CARD NUMBER */}
+              <label className='tracking-wider mt-6 mb-2'>CARD NUMBER</label>
+              <input 
+                type="text" 
+                placeholder="0000 0000 0000 0000"
+                maxLength={19}
+                {...register("cardNumber", { 
+                  required: "Card number is required", 
+                  pattern: {
+                    value: /^(\d{4} ){3}\d{4}$/,
+                    message: "Invalid format"
+                  } 
+                })}
+                onChange={(e) => {
+                  const formatted = formatCardNumber(e.target.value);
+                  setValue("cardNumber", formatted); // Actualiza el valor en el form
+                }}
+                className={`text-lg ${errors.cardNumber ? 'border-red-500' : 'border-gray-300'}`}
+              />
+              {errors.cardNumber && <span className="text-red-500 mt-2">{errors.cardNumber.message}</span>}
 
-                  <input 
-                    type="text" 
-                    {...register("expYear", {
-                      required: "Year is required",
-                      pattern: {
-                        value: /^(2[5-9]|[3-9][0-9])$/ ,
-                        message: "Invalid year"
-                      }
-                    })}
-                    onChange={(e) => {
-                      const formatted = e.target.value.replace(/\D/g, "").slice(0, 2);
-                      setValue("expYear", formatted); // Actualiza el valor en el form
-                    }}
-                    className='w-full text-lg' 
-                    placeholder='YY'
-                  />
+
+              {/* MONTH AND CVC */}
+              <div className="flex gap-4 mt-6">
+                <div className="flex-1">
+                  <label className="block mb-2 tracking-wider">EXP. DATE (MM/YY)</label>
+                  <div className='flex gap-2'>
+                    <input 
+                      type="text" 
+                      {...register("expMonth", {
+                        required: "Month is required",
+                        pattern: {
+                          value: /^(0[1-9]|1[0-2])$/,
+                          message: "Invalid month"
+                        }
+                      })}
+                      onChange={(e) => {
+                        const formatted = e.target.value.replace(/\D/g, "").slice(0, 2);
+                        setValue("expMonth", formatted); // Actualiza el valor en el form
+                      }}
+                      className='w-full text-lg' 
+                      placeholder='MM'
+                    />
+
+                    <input 
+                      type="text" 
+                      {...register("expYear", {
+                        required: "Year is required",
+                        pattern: {
+                          value: /^(2[5-9]|[3-9][0-9])$/ ,
+                          message: "Invalid year"
+                        }
+                      })}
+                      onChange={(e) => {
+                        const formatted = e.target.value.replace(/\D/g, "").slice(0, 2);
+                        setValue("expYear", formatted); // Actualiza el valor en el form
+                      }}
+                      className='w-full text-lg' 
+                      placeholder='YY'
+                    />
+                  </div>
+                    {errors.expMonth && <span className="text-red-500 mt-2 block">{errors.expMonth.message}</span>}
+                    {errors.expYear && <span className="text-red-500 mt-2">{errors.expYear.message}</span>}
+                    
                 </div>
-                  {errors.expMonth && <span className="text-red-500 mt-2 block">{errors.expMonth.message}</span>}
-                  {errors.expYear && <span className="text-red-500 mt-2">{errors.expYear.message}</span>}
-                  
+
+                <div className="flex-1">
+                  <label className="block mb-2 tracking-wider">CVC</label>
+                  <input 
+                    type="text" 
+                      {...register("cvc", {
+                        required: "CVC is required",
+                        pattern: {
+                          value: /^\d{3}$/,
+                          message: "Invalid CVC"
+                        }
+                      })}
+                    onChange={(e) => {
+                      const formatted = e.target.value.replace(/\D/g, "").slice(0, 3);
+                      setValue("cvc", formatted); // Actualiza el valor en el form
+                    }}
+                    className='w-full text-lg' 
+                    placeholder='e.g 123'
+                  />
+                  {errors.cvc && <span className="text-red-500 mt-2 block">{errors.cvc.message}</span>}
+                </div>
               </div>
 
-              <div className="flex-1">
-                <label className="block mb-2 tracking-wider">CVC</label>
-                <input 
-                  type="text" 
-                    {...register("cvc", {
-                      required: "CVC is required",
-                      pattern: {
-                        value: /^\d{3}$/,
-                        message: "Invalid CVC"
-                      }
-                    })}
-                  onChange={(e) => {
-                    const formatted = e.target.value.replace(/\D/g, "").slice(0, 3);
-                    setValue("cvc", formatted); // Actualiza el valor en el form
-                  }}
-                  className='w-full text-lg' 
-                  placeholder='e.g 123'
-                />
-                {errors.cvc && <span className="text-red-500 mt-2 block">{errors.cvc.message}</span>}
-              </div>
+              <button className='bg-[#21092f] text-white rounded-md p-3 mt-5 cursor-pointer'>Confirm</button>
             </div>
 
-            <button className='bg-[#21092f] text-white rounded-md p-3 mt-5 cursor-pointer'>Confirm</button>
-          </div>
-
-        </form>
-        ) : (
-          <div className="flex flex-col items-center text-center p-10 scale-125">
-            <img src={iconComplete} className="w-20 mb-8" />
-            <h2 className="text-4xl tracking-widest mb-4">THANK YOU!</h2>
-            <p className="text-gray-500 text-lg mb-8">We’ve added your card details</p>
-            <button 
-              onClick={() => setIsSubmitted(false)} 
-              className='bg-[#21092f] text-white rounded-md p-3 w-2/3'
-            >
-              Continue
-            </button>
-          </div>
-        )}
+          </form>
+          ) : (
+            <div className="flex flex-col items-center text-center p-10 scale-125">
+              <img src={iconComplete} className="w-20 mb-8" />
+              <h2 className="text-4xl tracking-widest mb-4">THANK YOU!</h2>
+              <p className="text-gray-500 text-lg mb-8">We’ve added your card details</p>
+              <button 
+                onClick={() => setIsSubmitted(false)} 
+                className='bg-[#21092f] text-white rounded-md p-3 w-2/3'
+              >
+                Continue
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
     
